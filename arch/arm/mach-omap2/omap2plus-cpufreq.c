6121de0fd9ea715c8308cd10f2ce208029caccdb
Eugen Mandrenko <ievgen.mandrenko@ti.com>
2012-04-09 14:22:02 +0300

    OMAP2PLUS: remove compilation warning
    without CONFIG_OMAP4_DUTY_CYCLE defined in kernel configuration
    file, the compilation warning happens. The patch removes the
    problem.
    
    Change-Id: I76c549f7cae1f28d923829bbbbd6d062b142f3ea
    Signed-off-by: Eugen Mandrenko <ievgen.mandrenko@ti.com>

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -473,8 +473,8 @@ static void __exit omap_duty_cooling_exit(void)
 
 #else
 
-static int __init omap_duty_cooling_init(void) { return 0; };
-static void __exit omap_duty_cooling_exit(void);
+static int __init omap_duty_cooling_init(void) { return 0; }
+static void __exit omap_duty_cooling_exit(void) { }
 
 #endif
 
