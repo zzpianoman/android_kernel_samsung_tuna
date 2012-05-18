e32b3f34c884ac0e148e645efc3235262fa22a52
Mike Turquette <mturquette@ti.com>
2012-05-18 13:56:53 +0300

    OMAP4: DPLL cascading: introduce DPLL_ABE low power bypass/cascade
    DPLL_ABE can output 196.608MHz when fed from the 32KHz timer if
    CM_CLKMODE_DPLL_ABE.REGM4XEN is set.  This unique property allows us to
    make DPLL_ABE the parent of DPLL_CORE and then drive DPLL_MPU and DPLL_IVA,
    all of which are in Low Power Bypass mode.  This allows the system to remain
    usable while consuming far less power than when all of the DPLLs are
    locked. The primary use case for this mode low power audio playback with the
    display turned off.
    
    Many caveats exist in this DPLL cascading mode:
    DSS operation is unsupported, MPU is limited to 196.608MHz, ABE runs at
    49.152MHz in LP mode and there are no doubt others we haven't figured
    out yet.
    
    [Andrii Tseglytskyi <andrii.tseglytskyi@ti.com>:
    Ported and adapted to p-android-omap-3.0]
    
    Change-Id: I63ce263dd793801b9b13daff4d1b5f8f326b6d98
    Signed-off-by: Mike Turquette <mturquette@ti.com>
    Signed-off-by: Andrii Tseglytskyi <andrii.tseglytskyi@ti.com>

--- arch/arm/mach-omap2/dvfs.c
+++ arch/arm/mach-omap2/dvfs.c
@@ -34,6 +34,9 @@
 #endif
 
 
+#ifdef CONFIG_OMAP4_DPLL_CASCADING
+#include <mach/omap4-common.h>
+#endif
 /**
  * DOC: Introduction
  * =================
@@ -978,6 +981,11 @@ int omap_device_scale(struct device *req_dev, struct device *target_dev,
 		dev_dbg(target_dev, "%s: pm is not ready yet\n", __func__);
 		return -EBUSY;
 	}
+
+#ifdef CONFIG_OMAP4_DPLL_CASCADING
+	if (omap4_is_in_dpll_cascading())
+		return -EBUSY;
+#endif
 
 	/* Lock me to ensure cross domain scaling is secure */
 	mutex_lock(&omap_dvfs_lock);
