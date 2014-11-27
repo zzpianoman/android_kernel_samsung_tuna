ee3186baf93a8f860e6569a6775a29c78d7dc97e
DirtyJerz <fluidflow00@gmail.com>
2014-11-26 22:16:07 -0500

    add envsetup.sh for standalone kernel compile

---
+++ envsetup.sh
@@ -0,0 +1,9 @@
+export ARCH=arm
+#AOSP
+export CROSS_COMPILE=arm-eabi-
+export PATH=/home/sysop/Android/WORKING_DIRECTORY/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin:$PATH
+#Linaro
+#export CROSS_COMPILE=arm-linux-androideabi-
+#export PATH=/home/sysop/Android/WORKING_DIRECOTRY/prebuilts/gcc/linux-x86/arm/android-toolchain-eabi/bin:$PATH
+#export PATH=/home/sysop/Android/WORKING_DIRECTORY/prebuilts/gcc/linux-x86/arm/android-toolchain-eabi/bin:$PATH
+
